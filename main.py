9ea899737912aa09a8ffe1147396c5e2681064e0
baileycarothers <44585056+baileycarothers@users.noreply.github.com>
2022-01-28 23:25:40 -0600

    Updates for ticket
    Fixed the load patient info as requested. also cleaned up some syntax, reorganized a bit, and left some tips/tricks

--- main.py
+++ main.py
@@ -1,54 +1,70 @@
 from tkinter import *
 from tkinter import messagebox
-from tkinter import simpledialog;
+from tkinter import simpledialog
 
 
-
-
+# Initial window setup
 tkWindow = Tk()
 tkWindow.geometry('800x600')
-size = tkWindow['bg']='white'
+size = tkWindow['bg'] = 'white'
 tkWindow.title('O.S.M.A')
 
 
-
+# Function Definitions
 def pInfo():
-    pnAnswer = simpledialog.askstring("Add New Patient", "Insert Name") 
-    paAnswer = simpledialog.askstring("Patient Age", "Insert Age") 
-    file1 = open("info.txt","w")
-    L = ["Name of patient is: ", pnAnswer, "."]
-    L2 = [
-        " \nAge of patient: ", paAnswer, ".",]
-    file1.writelines(L)
-    file1.writelines(L2)
+    pnAnswer = simpledialog.askstring("Add New Patient", "Insert Name")
+    paAnswer = simpledialog.askinteger("Patient Age", "Insert Age") # Changed this to askinteger
+    file1 = open("info.txt", "a")
+    L = ["Name of patient is: ", pnAnswer, "\n"]
+    L2 = ["Age of patient: ", paAnswer, "\n"]
+    file1.writelines(L + L2)
+
+    # readable simply returns true or false just fyi
     file1.readable()
-    
 
-
-BTN = Button(tkWindow,
-                text='Add New Patient',
-             height=5,
-             width=10,
-             command=pInfo)
-
-BTN.pack()
-BTN.place(x=0, y=0)
-    
 
 def showMsg():
-    file = open("info.txt","r")
+    # Redundant to open it twice
+    # file = open("info.txt", "r")
     with open('info.txt') as f:
-       data = file.read()
-    messagebox.showinfo(data)
+        # You open it as f so you call f.read
+        data = f.read()
+    # You have to specify the arguments if you are using anything past the first
+    # The first argument that showinfo takes in is the title, so it assumed title=data
+    messagebox.showinfo(title="Patient Info", message=data)
+
+
+def closeWindow():
+    tkWindow.destroy()
+
+
+# Button Creation
+BTN = Button(tkWindow,
+             text='Add New Patient',
+             # height=5,
+             # width=10,
+             command=pInfo)
 
 
 BTN2 = Button(tkWindow,
-                text='Load Patient Info',
-             height=5,
-             width=10,
-                command=showMsg)
+              text='Load Patient Info',
+              # height=5,
+              # width=10,
+              command=showMsg)
 
+BTN_QUIT = Button(tkWindow, text="Quit", command=closeWindow)
+
+
+# Packing
+BTN.pack()
 BTN2.pack()
-BTN2.place(x=0, y=125)
+BTN_QUIT.pack()
 
+# TODO: I highly recommend switching to a grid layout, easier to control where elements are :)
+# Placement
+BTN.place(x=0, y=0)
+BTN2.place(x=0, y=125)
+BTN_QUIT.place(x=700, y=500)
+
+# Run the window
 tkWindow.mainloop()
